fe860dd0e74b5aeddf029f5423e116eef768368b
Mike Marsden <marsdems@muohio.edu>
2012-08-26 18:12:20 -0400

    Added Functionality

--- src/CatPictureApp.cpp
+++ src/CatPictureApp.cpp
@@ -11,10 +11,18 @@ class CatPictureApp : public AppBasic {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+
+  private:
+	float rValue;
+	float gValue;
+	float bValue;
 };
 
 void CatPictureApp::setup()
 {
+	rValue = 0.0f;
+	gValue = 0.5f;
+	bValue = 1.0f;
 }
 
 void CatPictureApp::mouseDown( MouseEvent event )
@@ -23,12 +31,24 @@ void CatPictureApp::mouseDown( MouseEvent event )
 
 void CatPictureApp::update()
 {
+	rValue = rValue + 0.01f;
+	gValue = gValue + 0.02f;
+	bValue = bValue + 0.03f;
+	if (rValue > 1.0f) {
+		rValue = 0.0f;
+	}
+	if (gValue > 1.0f) {
+		gValue = 0.0f;
+	}
+	if (bValue > 1.0f) {
+		bValue = 0.0f;
+	}
 }
 
 void CatPictureApp::draw()
 {
 	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear( Color( rValue, bValue, gValue) ); 
 }
 
 CINDER_APP_BASIC( CatPictureApp, RendererGl )
